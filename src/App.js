178b179863cbe973673dbde45875836e90a27f49
Prem Mithawala <prem@coindelta.io>
2023-06-28 18:29:44 +0530

    names added

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>This is Pointsville India Team. </p>
+        <p>Smit, Shubham, Prem </p>
         <a
           className="App-link"
           href="https://reactjs.org"
